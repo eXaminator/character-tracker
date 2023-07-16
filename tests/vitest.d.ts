93726a395499bc7e2d9e791143db4e4a79b5cc74
Steven Weingärtner <sweingaertner@brainbits.net>
2023-07-16 13:27:11 +0200

    initial commit

---
+++ tests/vitest.d.ts
@@ -0,0 +1,6 @@
+import { type TestingLibraryMatchers } from '@testing-library/jest-dom/matchers.js';
+
+declare module 'vitest' {
+    interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
+    interface AsymmetricMatchersContaining extends TestingLibraryMatchers<T, void> {}
+}
